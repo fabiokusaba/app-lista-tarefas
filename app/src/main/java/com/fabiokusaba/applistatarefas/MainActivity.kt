7281d34b7a9774388d0fae5d8118ecc591bfa666
Fabio Kusaba <fabinhokb44@gmail.com>
2023-12-15 21:40:07 -0300

    Config Intent and FloatingActionButton Adicionar

--- app/src/main/java/com/fabiokusaba/applistatarefas/MainActivity.kt
+++ app/src/main/java/com/fabiokusaba/applistatarefas/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.fabiokusaba.applistatarefas
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.fabiokusaba.applistatarefas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.fabAdicionar.setOnClickListener {
+            val intent = Intent(this, AdicionarTarefaActivity::class.java)
+
+            startActivity(intent)
+        }
     }
 }
